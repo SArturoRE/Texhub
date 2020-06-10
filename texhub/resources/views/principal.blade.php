6eeabe6a301429fd3106fbad6ec6e7d74a2b8ef1
ItzMont <itz_mont09@outlook.com>
2020-06-10 13:24:16 -0500

    Se agrego poyecto laravel

---
+++ texhub/resources/views/principal.blade.php
@@ -0,0 +1,15 @@
+@extends('plantilla')
+
+@section('titulo')
+Principal
+@endsection
+
+@section('botones')
+<button>Hola desde seccion</button>
+@endsection
+
+@section('sec_cont')
+<div class="row">
+<h1>Hola Caro Mi Amor </h1>
+</div>
+@endsection
